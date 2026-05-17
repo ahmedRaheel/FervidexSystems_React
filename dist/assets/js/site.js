9e1ef5284efcea0e0d2078e0d0868c2302943d39
ahmedRaheel <raaheelahmed@outlook.com>
2026-05-17 22:59:13 +0500

    Update latest document

--- dist/assets/js/site.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  const current = location.pathname.split('/').pop() || 'index.html';
-  document.querySelectorAll('.nav-link').forEach(x => {
-    if (x.getAttribute('href') === current) x.classList.add('active');
-  });
-});
